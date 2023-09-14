df8dff7c663bd7467dacf93e0889f6bd8547b67a
shaoyuq3 <yuqing.shao@mail.utoronto.ca>
2023-09-13 23:44:45 -0400

    Task 5

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 }
