b979c1eccb0078495ce76f10d16ba5285f105bed
shaoyuq3 <yuqing.shao@mail.utoronto.ca>
2023-09-15 11:05:12 -0400

    new version

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,6 @@
 public class Multiples {
     public static void main(String[] args) {
         int num = 0;
-
         for (int i = 0; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 num++;
